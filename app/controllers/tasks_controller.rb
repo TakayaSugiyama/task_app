cc6909d200468633c9e576d32f50f4a13bb9358e
takayasugiyama <s.takaya1027@gmail.com>
2019-09-07 00:01:03 +0900

    タスク新規登録機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,41 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show]
+
   def index
     @tasks = Task.all.order(updated_at: :desc)
   end
 
   def new
+    @task = Task.new(flash[:task])
   end
 
-  def show
-  end
+  def show ;end
 
   def edit
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save 
+      redirect_to @task, notice: "タスク「#{@task.title}」を作成しました"
+    else 
+      redirect_to new_task_path, flash: {task: @task, error_messages: @task.errors.full_messages}
+    end
   end
 
   def destroy
   end
 
   def update
+  end
+
+  private 
+
+  def set_task 
+    @task = Task.find(params[:id])
+  end
+
+  def task_params 
+    params.require(:task).permit(:title,:content)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {maximum: 100}, presence: true
 end
