1dcf7e338926305a68e14a608f5013bdbb3bc9cf
takayasugiyama <s.takaya1027@gmail.com>
2019-09-09 16:36:15 +0900

    Null規約とdefault値をmigrationで設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_065028) do
+ActiveRecord::Schema.define(version: 2019_09_09_071648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2019_09_09_065028) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "deadline"
+    t.datetime "deadline", default: "2019-09-09 16:35:38", null: false
   end
 
 end
