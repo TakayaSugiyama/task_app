ebe2247f8823818fce89b77b99b8c0107a68f27e
takayasugiyama <s.takaya1027@gmail.com>
2019-09-10 10:05:44 +0900

    taskテーブルにstatusカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_071648) do
+ActiveRecord::Schema.define(version: 2019_09_10_010405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_09_09_071648) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deadline", default: "2019-09-09 16:35:38", null: false
+    t.integer "status"
   end
 
 end
