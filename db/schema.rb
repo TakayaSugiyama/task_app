e9b403aa48c13a74ce5e65f04f97b22aefa8cbc8
takayasugiyama <s.takaya1027@gmail.com>
2019-09-10 10:30:59 +0900

    statusカラムにnull規約とデフォルトの値を設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_010405) do
+ActiveRecord::Schema.define(version: 2019_09_10_011957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_09_10_010405) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deadline", default: "2019-09-09 16:35:38", null: false
-    t.integer "status"
+    t.integer "status", default: 2, null: false
   end
 
 end
