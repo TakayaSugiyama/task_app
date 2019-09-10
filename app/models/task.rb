14f29c5b3081fc348e282eb2de1cf001cb6859ec
takayasugiyama <s.takaya1027@gmail.com>
2019-09-10 10:35:38 +0900

    enumを設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,6 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, length: {maximum: 100}, presence: true
   validates :deadline, presence: true
+  # 0: 最優先, 1:優先, 2: 普通, 3:後回し で設定する
+  enum status: {top_prioryty: 0, priority: 1, default: 2, wait: 3}
 end
