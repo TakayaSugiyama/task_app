67bb2c35453e9c7bc795b05f01bf903ec6272355
takayasugiyama <s.takaya1027@gmail.com>
2019-09-11 15:34:19 +0900

    ユーザーモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_225238) do
+ActiveRecord::Schema.define(version: 2019_09_11_063111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2019_09_10_225238) do
     t.integer "priority", default: 2, null: false
     t.integer "condition", default: 0, null: false
     t.index ["title", "condition"], name: "index_tasks_on_title_and_condition"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
