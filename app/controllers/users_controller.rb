148a41433a44e6f9c6c4d8c95af9467c5bc56e48
takayasugiyama <s.takaya1027@gmail.com>
2019-09-11 17:45:59 +0900

    ユーザー登録機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
   def new
+    @user = User.new(flash[:user])
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save 
+      redirect_to @user,notice: "登録しました"
+    else 
+      redirect_to new_user_path, flash: {user: @user,error_messages:@user.errors.full_messages}
+    end
   end
 
-  def show
+  def show;end
+
+  private 
+
+  def set_user 
+    @user = User.find(params[:id])
+  end
+
+  def user_params 
+    params.require(:user).permit(:name,:password,:password_confirmation,:email)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :tasks
+
+  validates :name, presence: true, length: {in: 3..10}
+  validates :email, presence: true,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },uniqueness: true
+  validates :password, presence: true, length: {minimum: 6}, on: :create
 end
