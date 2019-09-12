2e29a06f01080e89c514c8aa68217835d6e8451c
takayasugiyama <s.takaya1027@gmail.com>
2019-09-12 20:37:22 +0900

    dependent destroyを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :tasks
+  has_many :tasks , dependent: :destroy
 
   validates :name, presence: true, length: {in: 3..10}
   validates :email, presence: true,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },uniqueness: true
