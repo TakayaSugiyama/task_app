2ecdd059211f598e897e0adf130e4e8bca38ed17
takayasugiyama <s.takaya1027@gmail.com>
2019-09-13 11:15:44 +0900

    ルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
-  resources :users, only: [:new,:show,:create]
   root "tasks#index"
   resources :tasks, except: :index
+
+  namespace :admin do  
+    resources :users
+  end
 end
