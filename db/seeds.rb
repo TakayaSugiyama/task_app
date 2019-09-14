7d2149d51987a07dc4adf606f47dd7aa15a5f635
takayasugiyama <s.takaya1027@gmail.com>
2019-09-14 13:19:31 +0900

    seedファイルを編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-user = User.create(name: "リラックマ", email: "relax@gmail.com", password: "relaxbear")
-#user = User.create(name: "コリラックマ", email: "koribear@gmail.com", password: "koribear")
+user = User.create(name: "リラックマ", email: "relax@gmail.com", password: "relaxbear", admin: true)
+user = User.create(name: "コリラックマ", email: "koribear@gmail.com", password: "koribear", admin: false)
 #user = User.create(name: "キイロイトリ", email: "kiiroitori@gmail.com", password: "kiiroitori")
