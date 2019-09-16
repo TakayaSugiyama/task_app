7b9a0558af12a33cfcc25d08532455c27901b412
takayasugiyama <s.takaya1027@gmail.com>
2019-09-16 14:19:35 +0900

    Add: label_relationsテーブルにインデックスを張る

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_050315) do
+ActiveRecord::Schema.define(version: 2019_09_16_051334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_09_16_050315) do
     t.integer "label_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["task_id", "label_id"], name: "index_label_relations_on_task_id_and_label_id"
   end
 
   create_table "labels", force: :cascade do |t|
