21394302ecb24401a85ca5542c110e1d0e506443
takayasugiyama <s.takaya1027@gmail.com>
2019-09-16 12:27:46 +0900

    Fix: 余計なroutingを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'forbidden',to: "errors#forbidden"
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
